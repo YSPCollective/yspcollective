3c77047e87e4e3a84fb514aa94bacca7e3e60a07
YSPCollective <info@yspcollective.com>
2026-03-26 21:37:00 +0000

    Update ysp-config.js

--- src/ysp-config.js
+++ src/ysp-config.js
@@ -7,4 +7,4 @@
  * to keep your API key private. For now this works fine for testing.
  */
 
-window.YSP_API_KEY = 'YOUR_API_KEY_HERE';
+window.YSP_API_KEY = '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';
